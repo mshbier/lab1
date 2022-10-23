d398974789fc1208a153fd02e9c6c163f094ac12
Mohammed Shbier <m.shbier@up.edu.ps>
2022-10-23 09:02:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using ServerApp.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(opts => {
+opts.UseSqlServer(builder.Configuration[
+"ConnectionStrings:DefaultConnection"]);
+opts.EnableSensitiveDataLogging(true);
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+var context = app.Services.CreateScope().ServiceProvider
+.GetRequiredService<DataContext>();
+// SeedData.SeedDatabase(context);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
